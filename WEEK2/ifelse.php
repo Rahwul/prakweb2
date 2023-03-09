3e5fa4faf8a86657de40378eb04f5649f4ee2ca2
Rahwul <rahwul.ihsan20@gmail.com>
2023-03-08 21:27:25 -0800

    WEEK2

---
+++ WEEK2/ifelse.php
@@ -0,0 +1,10 @@
+<?php
+$teman = "aldi";
+
+//cek terlebih dahulu, apakah aldi teman saya atau tidak
+if ($teman == "aldi"){
+    echo "aldi adalah teman saya";
+}else{
+    echo "aldi bukan teman saya";
+}
+?>
